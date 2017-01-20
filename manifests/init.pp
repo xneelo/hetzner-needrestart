a4cff7deeb8b2ca2cc1f66d821da060090cc3950
Jan Vonde <mail@jan-von.de>
2017-01-20 17:58:38 +0100

    adds new preferred_ui and disable_kernel_hints option

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,10 @@
 class needrestart(
-  $action         = $needrestart::params::action,
-  $ignorelist     = $needrestart::params::ignorelist,
-  $package_ensure = $needrestart::params::package_ensure,
-  $package_name   = $needrestart::params::package_name,
+  $action               = $needrestart::params::action,
+  $preferred_ui         = $needrestart::params::preferred_ui,
+  $ignorelist           = $needrestart::params::ignorelist,
+  $disable_kernel_hints = $needrestart::params::disable_kernel_hints,
+  $package_ensure       = $needrestart::params::package_ensure,
+  $package_name         = $needrestart::params::package_name,
 ) inherits needrestart::params {
 
   include install
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,8 @@
 class needrestart::params(
-  $action          = 'list',
-  $ignorelist      = {'#Apache'  => ['#apache2'], '#Backups' => ['#bareos-fd', '#bareos-sd']},
-  $package_ensure  = 'installed',
-  $package_name    = 'needrestart',
+  $action               = 'list',
+  $preferred_ui         = 'stdio',
+  $disable_kernel_hints = true,
+  $ignorelist           = {'#Apache'  => ['#apache2'], '#Backups' => ['#bareos-fd', '#bareos-sd']},
+  $package_ensure       = 'installed',
+  $package_name         = 'needrestart',
 ){}
