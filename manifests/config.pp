699a317da5fda2142cdbb3a7b1d459b1e0712221
Jan Vonde <mail@jan-von.de>
2017-01-20 17:47:26 +0100

    moves logic from needrestart_customisations.conf to needrestart.conf

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,12 +7,4 @@ class needrestart::config inherits needrestart {
     content => template('needrestart/needrestart.conf.erb'),
     require => Class['needrestart::install'],
   }
-
-  file {'/etc/needrestart/conf.d/customisations.conf':
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('needrestart/needrestart_customisations.conf.erb'),
-    require => Class['needrestart::install'],
-  }
 }
