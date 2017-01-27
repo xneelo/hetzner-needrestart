d076e464364387091e5693a537ceb1688b202316
Jan Vonde <jan.vonde@intranda.com>
2017-01-27 14:00:30 +0100

    makes notification of users about obsolete binaries configurable

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,11 @@
 class needrestart(
-  $action               = $needrestart::params::action,
-  $preferred_ui         = $needrestart::params::preferred_ui,
-  $ignorelist           = $needrestart::params::ignorelist,
-  $disable_kernel_hints = $needrestart::params::disable_kernel_hints,
-  $package_ensure       = $needrestart::params::package_ensure,
-  $package_name         = $needrestart::params::package_name,
+  $action                        = $needrestart::params::action,
+  $preferred_ui                  = $needrestart::params::preferred_ui,
+  $ignorelist                    = $needrestart::params::ignorelist,
+  $disable_kernel_hints          = $needrestart::params::disable_kernel_hints,
+  $notify_user_obsolete_binaries = needrestart::params::notify_user_obsolete_binaries,
+  $package_ensure                = $needrestart::params::package_ensure,
+  $package_name                  = $needrestart::params::package_name,
 ) inherits needrestart::params {
 
   include install
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,9 @@
 class needrestart::params(
-  $action               = 'list',
-  $preferred_ui         = 'stdio',
-  $disable_kernel_hints = true,
-  $ignorelist           = {},
-  $package_ensure       = 'installed',
-  $package_name         = 'needrestart',
+  $action                        = 'list',
+  $preferred_ui                  = 'stdio',
+  $disable_kernel_hints          = true,
+  $notify_user_obsolete_binaries = true,
+  $ignorelist                    = {},
+  $package_ensure                = 'installed',
+  $package_name                  = 'needrestart',
 ){}
