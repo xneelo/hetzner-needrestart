77b51d13dc3ce7af05b61d0135df755af128e802
Jan Vonde <mail@jan-von.de>
2017-01-28 15:00:21 +0100

    fixes typo in init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@ class needrestart(
   $preferred_ui                  = $needrestart::params::preferred_ui,
   $ignorelist                    = $needrestart::params::ignorelist,
   $disable_kernel_hints          = $needrestart::params::disable_kernel_hints,
-  $notify_user_obsolete_binaries = needrestart::params::notify_user_obsolete_binaries,
+  $notify_user_obsolete_binaries = $needrestart::params::notify_user_obsolete_binaries,
   $package_ensure                = $needrestart::params::package_ensure,
   $package_name                  = $needrestart::params::package_name,
 ) inherits needrestart::params {
