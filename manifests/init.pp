09ee608368b60a15af84a332c5281cb9a5b47635
walkleyn <walter@kleynscheldt.com>
2018-09-25 13:53:33 +0200

    Clean up code to only use overrides by the newly supplied hiera mecha… (#11)
    * Clean up code to only use overrides by the newly supplied hiera mechanism
    
    * Update README
    
    * Bump version and compatibility information
    
    * pdk validate module
    
    * Add dependency to conf.d dir

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,3 @@
-#
 # main class for needrestart
 #
 # Parameters:
@@ -15,17 +14,10 @@
 #     override_rc:
 #       'qr(^dbus)': 0
 #       'qr(^gdm)': 0
-#
 class needrestart(
-  $action                        = $needrestart::params::action,
-  $preferred_ui                  = $needrestart::params::preferred_ui,
-  $ignorelist                    = $needrestart::params::ignorelist,
-  $disable_kernel_hints          = $needrestart::params::disable_kernel_hints,
-  $notify_user_obsolete_binaries = $needrestart::params::notify_user_obsolete_binaries,
   $package_ensure                = $needrestart::params::package_ensure,
   $package_name                  = $needrestart::params::package_name,
   $configs                       = {},
-  $has_pam_systemd               = $needrestart::params::has_pam_systemd,
 ) inherits needrestart::params {
 
 
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,5 @@
+#Default parameter values for needrestart
 class needrestart::params(
-  $action                        = 'list',
-  $preferred_ui                  = 'stdio',
-  $disable_kernel_hints          = true,
-  $notify_user_obsolete_binaries = true,
-  $ignorelist                    = {},
   $package_ensure                = 'installed',
   $package_name                  = 'needrestart',
-  $has_pam_systemd               = false,
 ){}
