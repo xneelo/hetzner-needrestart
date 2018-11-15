f125b3857cf3f3f7dcc578be81a482f13ee320ea
Moritz Schneider <countsudoku@users.noreply.github.com>
2018-11-15 13:22:14 +0100

    Add spec test for various resources

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,14 +1,31 @@
 require 'spec_helper'
+default_content = <<~CONTENT
+  # managed with puppet (module needrestart)
+
+  $nrconf{default_value} = {
+  };
+  CONTENT
+
 describe 'needrestart' do
   context 'with default values for all parameters on Debian' do
     let(:facts) { { operatingsystem: 'Debian' } }
 
     it { is_expected.to contain_class('needrestart') }
+    it { is_expected.to contain_package('needrestart') }
+    it { is_expected.to contain_file('/etc/needrestart/conf.d/').with(
+      'ensure' => 'directory') }
+    it { is_expected.to contain_file('/etc/needrestart/conf.d/overrides.conf').with(
+      'content' => default_content) }
   end
 
   context 'with default values for all parameters on Ubuntu 16.04' do
     let(:facts) { { operatingsystem: 'Ubuntu', lsbdistrelease: '16.04' } }
 
     it { is_expected.to contain_class('needrestart') }
+    it { is_expected.to contain_package('needrestart') }
+    it { is_expected.to contain_file('/etc/needrestart/conf.d/').with(
+      'ensure' => 'directory') }
+    it { is_expected.to contain_file('/etc/needrestart/conf.d/overrides.conf').with(
+      'content' => default_content) }
   end
 end
