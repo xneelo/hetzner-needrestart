d5f799fe65cae112651f6053554c6c0ab19eb61b
Moritz Schneider <countsudoku@users.noreply.github.com>
2018-11-15 13:53:27 +0100

    Add spec tests to check the overrides.con config

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -6,6 +6,16 @@ default_content = <<~CONTENT
   };
   CONTENT
 
+test_content = <<~CONTENT
+  # managed with puppet (module needrestart)
+
+  $nrconf{default_value} = {
+      restart = 'l';
+      defno = 0;
+  };
+  CONTENT
+
+
 describe 'needrestart' do
   context 'with default values for all parameters on Debian' do
     let(:facts) { { operatingsystem: 'Debian' } }
@@ -18,6 +28,17 @@ describe 'needrestart' do
       'content' => default_content) }
   end
 
+  context 'with config values on Debian' do
+    let(:facts) { { operatingsystem: 'Debian' } }
+    let(:params) { {'configs' => { 'restart' => 'l', 'defno' => 0 } } }
+
+    it { is_expected.to contain_class('needrestart') }
+    it { is_expected.to contain_file('/etc/needrestart/conf.d/').with(
+      'ensure' => 'directory') }
+    it { is_expected.to contain_file('/etc/needrestart/conf.d/overrides.conf').with(
+      'content' => test_content) }
+  end
+
   context 'with default values for all parameters on Ubuntu 16.04' do
     let(:facts) { { operatingsystem: 'Ubuntu', lsbdistrelease: '16.04' } }
 
@@ -28,4 +49,17 @@ describe 'needrestart' do
     it { is_expected.to contain_file('/etc/needrestart/conf.d/overrides.conf').with(
       'content' => default_content) }
   end
+
+  context 'with config values on Ubuntu 18.04' do
+    let(:facts) { { operatingsystem: 'Ubuntu', lsbdistrelease: '18.04' } }
+    let(:params) { {'configs' => { 'restart' => 'l', 'defno' => 0 } } }
+
+    it { is_expected.to contain_class('needrestart') }
+    it { is_expected.to contain_package('needrestart') }
+    it { is_expected.to contain_file('/etc/needrestart/conf.d/').with(
+      'ensure' => 'directory') }
+    it { is_expected.to contain_file('/etc/needrestart/conf.d/overrides.conf').with(
+      'content' => test_content) }
+  end
+
 end
