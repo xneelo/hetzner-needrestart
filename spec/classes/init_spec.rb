1385fc559143d36b10daaa75dadf537124ea7977
Moritz Schneider <countsudoku@users.noreply.github.com>
2018-11-16 15:17:13 +0100

    Simplified unit tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -21,82 +21,56 @@ test_content = <<~CONTENT
 
 
 describe 'needrestart' do
-  context 'with default values for all parameters on Debian' do
-    let(:facts) { { operatingsystem: 'Debian' } }
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) do
+          facts
+        end
+        context 'with default values for all parameters' do
 
-    it { is_expected.to contain_class('needrestart') }
-    it { is_expected.to contain_class('needrestart::install') }
-    it { is_expected.to contain_class('needrestart::config') }
-    it { is_expected.to contain_package('needrestart') }
-    it { is_expected.to contain_file('/etc/needrestart/conf.d/').with(
-      'ensure' => 'directory') }
-    it { is_expected.to contain_file('/etc/needrestart/conf.d/overrides.conf').with(
-      'content' => default_content) }
-  end
+          it { is_expected.to contain_class('needrestart') }
+          it { is_expected.to contain_class('needrestart::install') }
+          it { is_expected.to contain_class('needrestart::config') }
+          it { is_expected.to contain_package('needrestart') }
+          it { is_expected.to contain_file('/etc/needrestart/conf.d/').with(
+            'ensure' => 'directory') }
+          it { is_expected.to contain_file('/etc/needrestart/conf.d/overrides.conf').with(
+            'content' => default_content) }
+        end
 
-  context 'with config values on Debian' do
-    let(:facts) { { operatingsystem: 'Debian' } }
-    let(:params) { {'configs' => {
-        'restart' => 'l',
-        'defno' => 0,
-        'blacklist' => [
-            'qr(^/usr/bin/sudo(\.dpkg-new)?$)',
-            'qr(^/sbin/(dhclient|dhcpcd5|pump|udhcpc)(\.dpkg-new)?$)',
-        ]} } }
+        context 'with config values' do
+          let(:params) { {'configs' => {
+              'restart' => 'l',
+              'defno' => 0,
+              'blacklist' => [
+                  'qr(^/usr/bin/sudo(\.dpkg-new)?$)',
+                  'qr(^/sbin/(dhclient|dhcpcd5|pump|udhcpc)(\.dpkg-new)?$)',
+              ]} } }
 
-    it { is_expected.to contain_class('needrestart') }
-    it { is_expected.to contain_file('/etc/needrestart/conf.d/').with(
-      'ensure' => 'directory') }
-    it { is_expected.to contain_file('/etc/needrestart/conf.d/overrides.conf').with(
-      'content' => test_content) }
-  end
+          it { is_expected.to contain_class('needrestart') }
+          it { is_expected.to contain_file('/etc/needrestart/conf.d/').with(
+            'ensure' => 'directory') }
+          it { is_expected.to contain_file('/etc/needrestart/conf.d/overrides.conf').with(
+            'content' => test_content) }
+        end
 
-  context 'with default values for all parameters on Ubuntu 16.04' do
-    let(:facts) { { operatingsystem: 'Ubuntu', lsbdistrelease: '16.04' } }
+        context 'with different package name' do
+          package_name = 'needrestart_pkg'
+          let(:params) { { 'package_name' => package_name, } }
 
-    it { is_expected.to contain_class('needrestart') }
-    it { is_expected.to contain_class('needrestart::install') }
-    it { is_expected.to contain_class('needrestart::config') }
-    it { is_expected.to contain_package('needrestart') }
-    it { is_expected.to contain_file('/etc/needrestart/conf.d/').with(
-      'ensure' => 'directory') }
-    it { is_expected.to contain_file('/etc/needrestart/conf.d/overrides.conf').with(
-      'content' => default_content) }
-  end
+          it { is_expected.to contain_package('needrestart').with(
+            'name' => package_name)}
+        end
 
-  context 'with config values on Ubuntu 18.04' do
-    let(:facts) { { operatingsystem: 'Ubuntu', lsbdistrelease: '18.04' } }
-    let(:params) { {'configs' => {
-        'restart' => 'l',
-        'defno' => 0,
-        'blacklist' => [
-            'qr(^/usr/bin/sudo(\.dpkg-new)?$)',
-            'qr(^/sbin/(dhclient|dhcpcd5|pump|udhcpc)(\.dpkg-new)?$)',
-        ]} } }
+        context 'with package removed' do
+          let(:params) { { 'package_ensure' => 'absent' } }
 
-    it { is_expected.to contain_class('needrestart') }
-    it { is_expected.to contain_package('needrestart') }
-    it { is_expected.to contain_file('/etc/needrestart/conf.d/').with(
-      'ensure' => 'directory') }
-    it { is_expected.to contain_file('/etc/needrestart/conf.d/overrides.conf').with(
-      'content' => test_content) }
-  end
-
-  context 'with different package name' do
-    package_name = 'needrestart_pkg'
-    let(:facts) { { operatingsystem: 'Debian' } }
-    let(:params) { { 'package_name' => package_name, } }
-
-    it { is_expected.to contain_package('needrestart').with(
-      'name' => package_name)}
-  end
-
-  context 'with package removed' do
-    let(:facts) { { operatingsystem: 'Debian' } }
-    let(:params) { { 'package_ensure' => 'absent' } }
-
-    it { is_expected.to contain_package('needrestart').with(
-      'ensure' => 'absent')}
+          it { is_expected.to contain_package('needrestart').with(
+            'ensure' => 'absent')}
+        end
+      end
+    end
   end
 
   context 'unsupported os' do
@@ -105,5 +79,4 @@ describe 'needrestart' do
     it { is_expected.not_to contain_class('needrestart::install') }
     it { is_expected.not_to contain_class('needrestart::config') }
   end
-
 end
