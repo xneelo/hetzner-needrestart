368a02c82ab3884838c65cae970f1ccad44f197f
countsudoku <countsudoku@users.noreply.github.com>
2018-11-26 12:39:50 +0100

    Bugfix default config and package params (#15)
    * Use the same logic for hash values as for normal values
    
    For a normal config value there is a special logic for different types of
    values (For example string get quoted). This was done only for the values of
    configs, but not if the value is in a hash. This is now fixed.
    
    * Add spec test for various resources
    
    * Add spec tests to check the overrides.con config
    
    * Add spec tests for rest of the params
    
    * Fix package params to use the configured params in the init class
    
    * Add spec test
    
    * Update to new pdk-template version
    
    * Fix array representation in default_value
    
    * Simplified unit tests
    
    * Add support for later ubuntu versions

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,7 +2,7 @@
 class needrestart::install inherits needrestart {
 
   package { 'needrestart':
-    ensure => $needrestart::params::package_ensure,
-    name   => $needrestart::params::package_name,
+    ensure => $needrestart::package_ensure,
+    name   => $needrestart::package_name,
   }
 }
