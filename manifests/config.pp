xneelo/hetzner-needrestart

commit 368a02c82ab3884838c65cae970f1ccad44f197f

Bugfix default config and package params (#15)
* Use the same logic for hash values as for normal values

For a normal config value there is a special logic for different types of
values (For example string get quoted). This was done only for the values of
configs, but not if the value is in a hash. This is now fixed.

* Add spec test for various resources

* Add spec tests to check the overrides.con config

* Add spec tests for rest of the params

* Fix package params to use the configured params in the init class

* Add spec test

* Update to new pdk-template version

* Fix array representation in default_value

* Simplified unit tests

* Add support for later ubuntu versions

templates/overrides.conf.epp
@@ -13,7 +13,15 @@
 <% } -%>
 <% if ( $value =~ Hash ) { -%>$nrconf{<%= $index %>} = {
 <% $value.each | $i, $v | { -%>
-    <%= $i %> => <%= $v %>,
+    <%- if ( $v =~ String ) { -%>
+    <%= $i %> = '<%= $v %>';
+    <%- } elsif ( $v =~ Array ) { -%>
+    <%= $i %> = [
+        <%= $v.join(",\n        ") %>
+    ];
+    <%- } else { -%>
+    <%= $i %> = <%= $v %>;
+    <%- } -%>
 <% } -%>
 };
 <% } -%>

.rubocop.yml
@@ -19,6 +19,10 @@ AllCops:
 Metrics/LineLength:
   Description: People have wide screens, use them.
   Max: 200
+GetText/DecorateString:
+  Description: We don't want to decorate test output.
+  Exclude:
+  - spec/*
 RSpec/BeforeAfterAll:
   Description: Beware of using after(:all) as it may cause state to leak between tests.
     A necessary evil in acceptance testing.

.travis.yml
@@ -13,10 +13,10 @@ script:
   - 'bundle exec rake $CHECK'
 bundler_args: --without system_tests
 rvm:
-  - 2.4.4
+  - 2.5.0
 env:
   global:
-    - BEAKER_PUPPET_COLLECTION=puppet5 PUPPET_GEM_VERSION="~> 5.0"
+    - BEAKER_PUPPET_COLLECTION=puppet6 PUPPET_GEM_VERSION="~> 6.0"
 matrix:
   fast_finish: true
   include:
@@ -24,6 +24,9 @@ matrix:
       env: CHECK="syntax lint metadata_lint check:symlinks check:git_ignore check:dot_underscore check:test_file rubocop"
     -
       env: CHECK=parallel_spec
+    -
+      env: PUPPET_GEM_VERSION="~> 5.0" CHECK=parallel_spec
+      rvm: 2.4.4
     -
       env: PUPPET_GEM_VERSION="~> 4.0" CHECK=parallel_spec
       rvm: 2.1.9

Gemfile
@@ -1,22 +1,15 @@
 source ENV['GEM_SOURCE'] || 'https://rubygems.org'
 
 def location_for(place_or_version, fake_version = nil)
-  if place_or_version =~ %r{\A(git[:@][^#]*)#(.*)}
-    [fake_version, { git: Regexp.last_match(1), branch: Regexp.last_match(2), require: false }].compact
-  elsif place_or_version =~ %r{\Afile:\/\/(.*)}
-    ['>= 0', { path: File.expand_path(Regexp.last_match(1)), require: false }]
+  git_url_regex = %r{\A(?<url>(https?|git)[:@][^#]*)(#(?<branch>.*))?}
+  file_url_regex = %r{\Afile:\/\/(?<path>.*)}
+
+  if place_or_version && (git_url = place_or_version.match(git_url_regex))
+    [fake_version, { git: git_url[:url], branch: git_url[:branch], require: false }].compact
+  elsif place_or_version && (file_url = place_or_version.match(file_url_regex))
+    ['>= 0', { path: File.expand_path(file_url[:path]), require: false }]
   else
     [place_or_version, { require: false }]
-  end
-end
-
-def gem_type(place_or_version)
-  if place_or_version =~ %r{\Agit[:@]}
-    :git
-  elsif !place_or_version.nil? && place_or_version.start_with?('file:')
-    :file
-  else
-    :gem
   end
 end
 
@@ -36,7 +29,6 @@ group :development do
 end
 
 puppet_version = ENV['PUPPET_GEM_VERSION']
-puppet_type = gem_type(puppet_version)
 facter_version = ENV['FACTER_GEM_VERSION']
 hiera_version = ENV['HIERA_GEM_VERSION']
 

appveyor.yml
@@ -1,5 +1,8 @@
 ---
 version: 1.1.x.{build}
+branches:
+  only:
+    - master
 skip_commits:
   message: /^\(?doc\)?.*/
 clone_depth: 10
@@ -29,6 +32,14 @@ environment:
     -
       PUPPET_GEM_VERSION: ~> 5.0
       RUBY_VERSION: 24-x64
+      CHECK: parallel_spec
+    -
+      PUPPET_GEM_VERSION: ~> 6.0
+      RUBY_VERSION: 25
+      CHECK: parallel_spec
+    -
+      PUPPET_GEM_VERSION: ~> 6.0
+      RUBY_VERSION: 25-x64
       CHECK: parallel_spec
 matrix:
   fast_finish: true

manifests/install.pp
@@ -2,7 +2,7 @@
 class needrestart::install inherits needrestart {
 
   package { 'needrestart':
-    ensure => $needrestart::params::package_ensure,
-    name   => $needrestart::params::package_name,
+    ensure => $needrestart::package_ensure,
+    name   => $needrestart::package_name,
   }
 }

spec/classes/init_spec.rb
@@ -1,14 +1,82 @@
 require 'spec_helper'
-describe 'needrestart' do
-  context 'with default values for all parameters on Debian' do
-    let(:facts) { { operatingsystem: 'Debian' } }
+default_content = <<~CONTENT
+  # managed with puppet (module needrestart)
 
-    it { is_expected.to contain_class('needrestart') }
+  $nrconf{default_value} = {
+  };
+  CONTENT
+
+test_content = <<~CONTENT
+  # managed with puppet (module needrestart)
+
+  $nrconf{default_value} = {
+      restart = 'l';
+      defno = 0;
+      blacklist = [
+          qr(^/usr/bin/sudo(\\.dpkg-new)?$),
+          qr(^/sbin/(dhclient|dhcpcd5|pump|udhcpc)(\\.dpkg-new)?$)
+      ];
+  };
+  CONTENT
+
+
+describe 'needrestart' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) do
+          facts
+        end
+        context 'with default values for all parameters' do
+
+          it { is_expected.to contain_class('needrestart') }
+          it { is_expected.to contain_class('needrestart::install') }
+          it { is_expected.to contain_class('needrestart::config') }
+          it { is_expected.to contain_package('needrestart') }
+          it { is_expected.to contain_file('/etc/needrestart/conf.d/').with(
+            'ensure' => 'directory') }
+          it { is_expected.to contain_file('/etc/needrestart/conf.d/overrides.conf').with(
+            'content' => default_content) }
+        end
+
+        context 'with config values' do
+          let(:params) { {'configs' => {
+              'restart' => 'l',
+              'defno' => 0,
+              'blacklist' => [
+                  'qr(^/usr/bin/sudo(\.dpkg-new)?$)',
+                  'qr(^/sbin/(dhclient|dhcpcd5|pump|udhcpc)(\.dpkg-new)?$)',
+              ]} } }
+
+          it { is_expected.to contain_class('needrestart') }
+          it { is_expected.to contain_file('/etc/needrestart/conf.d/').with(
+            'ensure' => 'directory') }
+          it { is_expected.to contain_file('/etc/needrestart/conf.d/overrides.conf').with(
+            'content' => test_content) }
+        end
+
+        context 'with different package name' do
+          package_name = 'needrestart_pkg'
+          let(:params) { { 'package_name' => package_name, } }
+
+          it { is_expected.to contain_package('needrestart').with(
+            'name' => package_name)}
+        end
+
+        context 'with package removed' do
+          let(:params) { { 'package_ensure' => 'absent' } }
+
+          it { is_expected.to contain_package('needrestart').with(
+            'ensure' => 'absent')}
+        end
+      end
+    end
   end
 
-  context 'with default values for all parameters on Ubuntu 16.04' do
-    let(:facts) { { operatingsystem: 'Ubuntu', lsbdistrelease: '16.04' } }
-
+  context 'unsupported os' do
+    let(:facts) { { operatingsystem: 'UnsupportedOs' } }
     it { is_expected.to contain_class('needrestart') }
+    it { is_expected.not_to contain_class('needrestart::install') }
+    it { is_expected.not_to contain_class('needrestart::config') }
   end
 end

spec/default_facts.yml
@@ -2,7 +2,7 @@
 #
 # Facts specified here will override the values provided by rspec-puppet-facts.
 ---
-concat_basedir: "/tmp"
+concat_basedir: ""
 ipaddress: "172.16.254.254"
 is_pe: false
 macaddress: "AA:AA:AA:AA:AA:AA"

commit b8cb945dd7ed42bf9bdb8a56bf2ec673d92496ee

Fix bugs and stop forcing user behaviour (#21)
* Fix for #17, this was a bug introduced in #15

When the index is a hash the override_rc subsection uses rockets and commas, not equal signs and semi-colons

Also, restyle the module to not force our hiera lookups onto the consumer, but allow them to pass a Hash directly to the needrestart class like below.

```
class { 'needrestart':
  configs => $config_overrides
}
```

* Remove un-used .erb, functionality was replaced in #10

manifests/config.pp
@@ -1,22 +1,8 @@
 #
 # class to configure needrestart
 #
-class needrestart::config ( $config_overrides = lookup('needrestart::configs', Hash, 'deep', { 'default_value' => $needrestart::configs})
-  ) inherits needrestart {
-
-  file {'/etc/needrestart/conf.d/':
-    ensure  => 'directory',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    require => Class['needrestart::install'],
-    purge   => true,
-    recurse => true,
-  }
-
-  file {'/etc/needrestart/conf.d/README.needrestart':
-    require => [File['/etc/needrestart/conf.d/'],Class['needrestart::install']],
-  }
+class needrestart::config ( Hash $config_overrides
+  ){
 
   file {'/etc/needrestart/conf.d/overrides.conf':
     owner   => 'root',

manifests/init.pp
@@ -15,11 +15,10 @@
 #       'qr(^dbus)': 0
 #       'qr(^gdm)': 0
 class needrestart(
+  Hash $configs                  = {},
   $package_ensure                = $needrestart::params::package_ensure,
   $package_name                  = $needrestart::params::package_name,
-  $configs                       = {},
 ) inherits needrestart::params {
-
 
   $install = false
 
@@ -42,6 +41,21 @@ class needrestart(
 
   if $_install {
     include needrestart::install
-    include needrestart::config
+
+    file {'/etc/needrestart/conf.d/':
+      ensure  => 'directory',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755',
+      require => Class['needrestart::install'],
+      purge   => true,
+      recurse => true,
+    }
+
+    unless $configs == {} {
+      class { 'needrestart::config':
+        config_overrides => $configs,
+      }
+    }
   }
 }

templates/needrestart.conf.erb
@@ -1,146 +1,0 @@
-
-# needrestart - Restart daemons after library updates.
-#
-# Authors:
-#   Thomas Liske <thomas@fiasko-nw.net>
-#
-# Copyright Holder:
-#   2013 - 2017 (C) Thomas Liske [http://fiasko-nw.net/~thomas/]
-#
-# License:
-#   This program is free software; you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation; either version 2 of the License, or
-#   (at your option) any later version.
-#
-
-# This is the configuration file of needrestart. This is perl syntax.
-# needrstart uses reasonable default values, you might not need to
-# change anything.
-
-# Verbosity:
-#  0 => quiet
-#  1 => normal (default)
-#  2 => verbose
-#$nrconf{verbosity} = 2;
-
-# Path of the package manager hook scripts.
-#$nrconf{hook_d} = '/etc/needrestart/hook.d';
-
-# Path of user notification scripts.
-#$nrconf{notify_d} = '/etc/needrestart/notify.d';
-
-# If needrestart detects systemd it assumes that you use systemd's pam module.
-# This allows needrestart to easily detect user session. In case you use
-# systemd *without* pam_systemd.so you should set has_pam_systemd to false
-# to enable legacy session detection!
-<% if @has_pam_systemd == true -%>
-$nrconf{has_pam_systemd} = 1;
-<% else -%>
-#$nrconf{has_pam_systemd} = 0;
-<% end -%>
-
-# Disable sending notifications to user sessions running obsolete binaries
-# using scripts from $nrconf{notify_d}.
-#$nrconf{sendnotify} = 0;
-<% if @notify_user_obsolete_binaries == true -%>
-$nrconf{sendnotify} = 1;
-<% else -%>
-$nrconf{sendnotify} = 0;
-<% end -%>
-
-
-# Restart services (l)ist only, (i)nteractive or (a)utomatically.
-$nrconf{restart} = '<%= @action.chars.first %>';
-
-# Use prefered UI package.
-$nrconf{ui} = 'NeedRestart::UI::<%= @preferred_ui %>';
-
-# Change default answer to 'no' in (i)nteractive mode.
-#$nrconf{defno} = 1;
-
-# Blacklist binaries (list of regex).
-$nrconf{blacklist} = [
-    # ignore sudo (not a daemon)
-    qr(^/usr/bin/sudo(\.dpkg-new)?$),
-
-    # ignore DHCP clients
-    qr(^/sbin/(dhclient|dhcpcd5|pump|udhcpc)(\.dpkg-new)?$),
-
-    # ignore apt-get (Debian Bug#784237)
-    qr(^/usr/bin/apt-get(\.dpkg-new)?$),
-];
-
-# Blacklist services (list of regex) - USE WITH CARE.
-# You should prefere to put services to $nrconf{override_rc} instead.
-# Any service listed in $nrconf{blacklist_rc} we be ignored completely!
-#$nrconf{blacklist_rc} = [
-#];
-
-# Override service default selection (hash of regex).
-$nrconf{override_rc} = {
-    # DBus
-    qr(^dbus) => 0,
-
-    # display managers
-    qr(^gdm) => 0,
-    qr(^kdm) => 0,
-    qr(^nodm) => 0,
-    qr(^sddm) => 0,
-    qr(^wdm) => 0,
-    qr(^xdm) => 0,
-    qr(^lightdm) => 0,
-
-    # networking stuff
-    qr(^network-manager) => 0,
-    qr(^NetworkManager) => 0,
-    qr(^wpa_supplicant) => 0,
-    qr(^openvpn) => 0,
-    qr(^quagga) => 0,
-    qr(^tinc) => 0,
-
-    # gettys
-    qr(^getty@.+\.service) => 0,
-
-    # systemd --user
-    qr(^user@\d+\.service) => 0,
-
-    # misc
-    qr(^zfs-fuse) => 0,
-    qr(^mythtv-backend) => 0,
-
-    # workaround for broken systemd-journald
-    # (see also Debian Bug#771122 & #771254)
-    qr(^systemd-journald) => 0,
-
-    # more systemd stuff
-    # (see also Debian Bug#784238 & #784437)
-    qr(^emergency\.service$) => 0,
-    qr(^rescue\.service$) => 0,
-
-    # don't restart systemd-logind, see #798097
-    qr(^systemd-logind) => 0,
-};
-
-# Disable interpreter scanners.
-#$nrconf{interpscan} = 0;
-
-# Enable/disable hints on pending kernel upgrades:
-#  1: requires the user to acknowledge pending kernels
-#  0: disable kernel checks completely
-# -1: print kernel hints to stderr only
-<% if @disable_kernel_hints == true -%>
-$nrconf{kernelhints} = 0;
-<% else -%>
-$nrconf{kernelhints} = 1;
-<% end -%>
-
-
-# Read additional config snippets.
-if(-d q(/etc/needrestart/conf.d)) {
-      foreach my $fn (sort </etc/needrestart/conf.d/*.conf>) {
-	      print STDERR "$LOGPREF eval $fn\n" if($nrconf{verbose});
-	      eval do { local(@ARGV, $/) = $fn; <>};
-	      die "Error parsing $fn: $@" if($@);
-      }
-}

templates/overrides.conf.epp
@@ -14,13 +14,13 @@
 <% if ( $value =~ Hash ) { -%>$nrconf{<%= $index %>} = {
 <% $value.each | $i, $v | { -%>
     <%- if ( $v =~ String ) { -%>
-    <%= $i %> = '<%= $v %>';
+    <%= $i %> => '<%= $v %>',
     <%- } elsif ( $v =~ Array ) { -%>
     <%= $i %> = [
         <%= $v.join(",\n        ") %>
     ];
     <%- } else { -%>
-    <%= $i %> = <%= $v %>;
+    <%= $i %> => <%= $v %>,
     <%- } -%>
 <% } -%>
 };